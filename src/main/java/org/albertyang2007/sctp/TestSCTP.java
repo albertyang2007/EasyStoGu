59e1f3141b665232508f05ef7b55ba38528f02d7
albertyang2007 <albertyang2007@gmail.com>
2014-01-20 23:28:12 +0800

    initial submit.

---
+++ src/main/java/org/albertyang2007/sctp/TestSCTP.java
@@ -0,0 +1,9 @@
+package org.albertyang2007.sctp;
+
+import com.sun.nio.sctp.SctpChannel;
+
+public class TestSCTP {
+	public static void main(String[] args) throws Exception {
+		 SctpChannel sc =  SctpChannel.open();
+	}
+}
