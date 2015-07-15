5ccb0c6422b127386eb392fbeda1e4c2c0623328
eyaweiw <albert.yang@ericsson.com>
2015-07-15 11:50:56 +0800

    add

--- Portal/src/main/java/org/easystogu/portal/HomeEndPoint.java
+++ Portal/src/main/java/org/easystogu/portal/HomeEndPoint.java
@@ -1,14 +1,39 @@
 package org.easystogu.portal;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
-@Controller
+import org.easystogu.runner.DailySelectionRunner;
+import org.easystogu.runner.DailyUpdateOverAllRunner;
+import org.easystogu.sina.runner.RealtimeDisplayStockPriceRunner;
+
+@Path("/home")
 public class HomeEndPoint {
-    @RequestMapping(value = "/")
-    @ResponseBody()
-    public String index() {
-        return "Hello EasyStoGu";
-    }
+	@GET
+	@Path("/hello")
+	public Response test() {
+		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/DailyUpdateOverAllRunner")
+	public String dailyUpdateOverAllRunner() {
+		DailyUpdateOverAllRunner.main(null);
+		return "DailyUpdateOverAllRunner already running, please check folder result.";
+	}
+	
+	@GET
+	@Path("/DailySelectionRunner")
+	public String dailySelectionRunner() {
+		DailySelectionRunner.main(null);
+		return "DailySelectionRunner already running, please check folder result.";
+	}
+	
+	@GET
+	@Path("/RealtimeDisplayStockPriceRunner")
+	public String realtimeDisplayStockPriceRunner() {
+		RealtimeDisplayStockPriceRunner.main(null);
+		return "RealtimeDisplayStockPriceRunner already running, please check console result.";
+	}
 }
