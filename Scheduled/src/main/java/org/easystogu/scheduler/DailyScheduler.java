29c1e3d261040125963169ad979fad0bac00bbaa
eyaweiw <albert.yang@ericsson.com>
2015-07-17 16:05:47 +0800

    add

--- Scheduled/src/main/java/org/easystogu/scheduler/DailyScheduler.java
+++ Scheduled/src/main/java/org/easystogu/scheduler/DailyScheduler.java
@@ -7,33 +7,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
-@EnableAsync
 @EnableScheduling
 public class DailyScheduler implements SchedulingConfigurer {
 	private static Logger logger = LogHelper.getLogger(DailyScheduler.class);
 	@Autowired
-	@Qualifier("dailyScheduler")
-	private TaskScheduler dailyScheduler;
+	@Qualifier("taskScheduler")
+	private TaskScheduler taskScheduler;
 
 	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-		taskRegistrar.setScheduler(dailyScheduler);
+		taskRegistrar.setScheduler(taskScheduler);
 	}
 
 	// refer to:
 	// http://www.quartz-scheduler.org/documentation/quartz-1.x/tutorials/crontrigger
-	@Scheduled(cron = "0 0/31 9-16 * * MON-FRI")
-	public void every30MinutesUpdateOverAllRunner() {
+	// run at 9:59,10:59
+	@Scheduled(cron = "0 59 9-10 * * MON-FRI")
+	public void morningUpdateOverAllRunner() {
+		this.DailyUpdateOverAllRunner();
+	}
+
+	// run at 13:30, 14:30
+	@Scheduled(cron = "0 30 13-14 * * MON-FRI")
+	public void afternoonUpdateOverAllRunner() {
+		this.DailyUpdateOverAllRunner();
+	}
+
+	// run at 11:35
+	@Scheduled(cron = "0 35 11 * * MON-FRI")
+	public void noonUpdateOverAllRunner() {
+		this.DailyUpdateOverAllRunner();
+	}
+
+	// run at 15:05
+	@Scheduled(cron = "0 05 15 * * MON-FRI")
+	public void FinallyUpdateOverAllRunner() {
+		this.DailyUpdateOverAllRunner();
+	}
+
+	private void DailyUpdateOverAllRunner() {
+		logger.info("DailyUpdateOverAllRunner already running, please check folder result.");
+		System.out
+				.println("DailyUpdateOverAllRunner already running, please check folder result.");
+
 		Thread t = new Thread(new DailyUpdateOverAllRunner());
 		t.start();
-		logger.info("every30MinutesUpdateOverAllRunner already running, please check folder result.");
-		System.out
-				.println("every30MinutesUpdateOverAllRunner already running, please check folder result.");
 	}
+
+	// @Scheduled(cron = "0 0/5 9-20 * * MON-FRI")
+	// public void test1() {
+	// logger.info("test 1: every 5 mins during 9-20 between MON-FRI");
+	// System.out.println("test 1: every 5 mins during 9-20 between MON-FRI");
+	// }
+
+	// @Scheduled(fixedDelay = 60 * 1000)
+	// public void test2() {
+	// logger.info("test 2: every 1 mins");
+	// System.out.println("test 2: every 1 mins");
+	// }
 }
