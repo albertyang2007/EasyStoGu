63df8ee65f8222b2e5f1faf9001f762d39b316a0
eyaweiw <albert.yang@ericsson.com>
2016-03-24 15:36:38 +0800

    add

--- DatabaseLib/src/main/java/org/easystogu/db/sync/StockPriceTableExport.java
+++ DatabaseLib/src/main/java/org/easystogu/db/sync/StockPriceTableExport.java
@@ -33,7 +33,7 @@ public class StockPriceTableExport {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         StockPriceTableExport ins = new StockPriceTableExport();
-        ins.exportToFile("2016-03-21");
+        ins.exportToFile("2016-03-24");
     }
 
 }
