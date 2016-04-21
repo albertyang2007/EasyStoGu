1c8eea5ed32bca3d7a077ff8cb792e658c9fc599
eyaweiw <albert.yang@ericsson.com>
2016-04-21 18:36:13 +0800

    add

--- Analyse/src/main/java/org/easystogu/report/HistoryAnalyseReport.java
+++ Analyse/src/main/java/org/easystogu/report/HistoryAnalyseReport.java
@@ -184,34 +184,31 @@ public class HistoryAnalyseReport {
         IndProcessHelper.processDayList(overDayList);
         IndProcessHelper.processWeekList(overWeekList);
 
-        HistoryReportDetailsVO reportVO = null;
         for (int index = 120; index < overDayList.size(); index++) {
             StockSuperVO superVO = overDayList.get(index);
 
             // buy point
-            if (reportVO == null) {
-                String startDate = overDayList.get(index - 120).priceVO.date;
-                String endDate = overDayList.get(index).priceVO.date;
-                // System.out.println(startDate + " ~~ " + endDate);
-                // include the startDate, not include the endDate
-                List<StockSuperVO> subOverWeekList = this.getSubWeekVOList(overWeekList, startDate, endDate);
-                // System.out.println(subOverWeekList.get(0).priceVO.date +
-                // " week "
-                // + subOverWeekList.get(subOverWeekList.size() -
-                // 1).priceVO.date);
+            String startDate = overDayList.get(index - 120).priceVO.date;
+            String endDate = overDayList.get(index).priceVO.date;
+            // System.out.println(startDate + " ~~ " + endDate);
+            // include the startDate, not include the endDate
+            List<StockSuperVO> subOverWeekList = this.getSubWeekVOList(overWeekList, startDate, endDate);
+            // System.out.println(subOverWeekList.get(0).priceVO.date +
+            // " week "
+            // + subOverWeekList.get(subOverWeekList.size() -
+            // 1).priceVO.date);
 
-                List<StockSuperVO> subOverDayList = overDayList.subList(index - 120, index + 1);
+            List<StockSuperVO> subOverDayList = overDayList.subList(index - 120, index + 1);
 
-                // System.out.println(subOverDayList.get(0).priceVO.date +
-                // " day "
-                // + subOverDayList.get(subOverDayList.size() -
-                // 1).priceVO.date);
+            // System.out.println(subOverDayList.get(0).priceVO.date +
+            // " day "
+            // + subOverDayList.get(subOverDayList.size() -
+            // 1).priceVO.date);
 
-                if (combineAanalyserHelper.isConditionSatisfy(checkPoint, subOverDayList, subOverWeekList)) {
-                    reportVO = new HistoryReportDetailsVO(overDayList);
-                    reportVO.setBuyPriceVO(superVO.priceVO);//must keep it
-                    historyReportList.add(reportVO);
-                }
+            if (combineAanalyserHelper.isConditionSatisfy(checkPoint, subOverDayList, subOverWeekList)) {
+                HistoryReportDetailsVO reportVO = new HistoryReportDetailsVO(overDayList);
+                reportVO.setBuyPriceVO(superVO.priceVO);//must keep it
+                historyReportList.add(reportVO);
             }
         }
 
@@ -405,9 +402,9 @@ public class HistoryAnalyseReport {
         // reporter.searchAllStockIdAnalyseHistoryCheckPoint(checkPoint);
         // }
         // }
-        //reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseI_GuanCha);
-        //reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseII_JianCang);
+        reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseI_GuanCha);
+        reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseII_JianCang);
         reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseIII_ChiGu);
-        //reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseVI_JianCang);
+        reporter.searchAllStockIdStatisticsCheckPoint(DailyCombineCheckPoint.Trend_PhaseVI_JianCang);
     }
 }
