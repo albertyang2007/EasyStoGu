67db09869a06ebb84dd06bdd01efa04e1205889c
albertyang2007 <albertyang2007@gmail.com>
2016-05-02 12:48:12 +0800

    add

--- Indicator/src/main/java/org/easystogu/indicator/runner/history/HistortOverAllRunner.java
+++ Indicator/src/main/java/org/easystogu/indicator/runner/history/HistortOverAllRunner.java
@@ -2,15 +2,12 @@ package org.easystogu.indicator.runner.history;
 
 public class HistortOverAllRunner {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// pls limit the startDay to avoid too much time
-		// day
 		HistoryMacdCountAndSaveDBRunner.main(args);
 		HistoryKDJCountAndSaveDBRunner.main(args);
 		HistoryBollCountAndSaveDBRunner.main(args);
 		HistoryShenXianCountAndSaveDBRunner.main(args);
 		HistoryQSDDCountAndSaveDBRunner.main(args);
-		HistoryYiMengBSCountAndSaveDBRunner.main(args);
+		// HistoryYiMengBSCountAndSaveDBRunner.main(args);
 		// HistoryMai1Mai2CountAndSaveDBRunner.main(args);
 		// HistoryXueShi2CountAndSaveDBRunner.main(args);
 		// HistoryZhuliJinChuCountAndSaveDBRunner.main(args);
