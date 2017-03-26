f148f09fa67b4cf94f614f5034470de09322b25e
Albert.Yang <albertyang2007@gmail.com>
2017-03-27 00:43:15 +0800

    add a bug

--- Portal/src/main/java/org/easystogu/portal/init/TrendModeLoader.java
+++ Portal/src/main/java/org/easystogu/portal/init/TrendModeLoader.java
@@ -1,6 +1,7 @@
 package org.easystogu.portal.init;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,12 +9,12 @@ import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import net.sf.json.JSONObject;
-
 import org.easystogu.file.TextFileSourceHelper;
 import org.easystogu.trendmode.vo.SimplePriceVO;
 import org.easystogu.trendmode.vo.TrendModeVO;
 import org.springframework.stereotype.Component;
+
+import net.sf.json.JSONObject;
 
 @Component
 public class TrendModeLoader {
@@ -57,6 +58,16 @@ public class TrendModeLoader {
         String[] str = this.trendModeMap.keySet().toArray(new String[0]);
         for (String s : str) {
             names.add(s);
+        }
+        return names;
+    }
+    
+    //display error when have Chinese
+    public List<String> getAllDisplayNames() {
+        List<String> names = new ArrayList<String>();
+        Collection<TrendModeVO> list = this.trendModeMap.values();
+        for (TrendModeVO tmo : list) {
+            names.add(tmo.name);
         }
         return names;
     }
