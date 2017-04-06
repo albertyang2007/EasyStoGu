d95e9f8b3f19f71e939fab201edbd778f5ec69c0
Albert.Yang <albertyang2007@gmail.com>
2017-04-06 18:14:50 +0800

    add

--- Cache/src/main/java/org/easystogu/cache/runner/AllCacheRunner.java
+++ Cache/src/main/java/org/easystogu/cache/runner/AllCacheRunner.java
@@ -1,6 +1,9 @@
 package org.easystogu.cache.runner;
 
+import org.easystogu.cache.CheckPointDailySelectionTableCache;
 import org.easystogu.cache.CheckPointStatisticsCache;
+import org.easystogu.cache.CommonViewCache;
+import org.easystogu.cache.ConfigurationServiceCache;
 import org.easystogu.cache.StockIndicatorCache;
 import org.easystogu.cache.StockPriceCache;
 
@@ -8,11 +11,17 @@ public class AllCacheRunner implements Runnable {
 	private CheckPointStatisticsCache checkPointStatisticsCache = CheckPointStatisticsCache.getInstance();
 	private StockIndicatorCache stockIndicatorCache = StockIndicatorCache.getInstance();
 	private StockPriceCache stockPriceCache = StockPriceCache.getInstance();
+	private CheckPointDailySelectionTableCache checkPointDailySelectionTableCache = CheckPointDailySelectionTableCache.getInstance();
+	private CommonViewCache commonViewCache = CommonViewCache.getInstance();
+	private ConfigurationServiceCache configurationServiceCache = ConfigurationServiceCache.getInstance();
 
 	public void refreshAll() {
 		this.checkPointStatisticsCache.refreshAll();
 		this.stockIndicatorCache.refreshAll();
 		this.stockPriceCache.refreshAll();
+		this.checkPointDailySelectionTableCache.refreshAll();
+		this.commonViewCache.refreshAll();
+		this.configurationServiceCache.refreshAll();
 	}
 
 	public void run() {
