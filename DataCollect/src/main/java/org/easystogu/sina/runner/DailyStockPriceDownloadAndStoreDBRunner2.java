a3372df9ba441f2b9bf5c9a86e1c079d27ae0635
Albert.Yang <albert.yang@ericsson.com>
2017-10-09 23:54:24 +0800

    fix latest_20 query issue

--- DataCollect/src/main/java/org/easystogu/sina/runner/DailyStockPriceDownloadAndStoreDBRunner2.java
+++ DataCollect/src/main/java/org/easystogu/sina/runner/DailyStockPriceDownloadAndStoreDBRunner2.java
@@ -18,114 +18,120 @@ import org.easystogu.utils.Strings;
 //it will get all the stockId from the web, including the new on board stockId
 public class DailyStockPriceDownloadAndStoreDBRunner2 implements Runnable {
 
-	// private static Logger logger =
-	// LogHelper.getLogger(DailyStockPriceDownloadAndStoreDBRunner2.class);
-	private CompanyInfoFileHelper stockConfig = CompanyInfoFileHelper.getInstance();
-	private StockPriceTableHelper stockPriceTable = StockPriceTableHelper.getInstance();
-	//private HouFuQuanStockPriceTableHelper houfuquanStockPriceTable = HouFuQuanStockPriceTableHelper.getInstance();
-	private QianFuQuanStockPriceTableHelper qianfuquanStockPriceTable = QianFuQuanStockPriceTableHelper.getInstance();
-	//private ScheduleActionTableHelper scheduleActionTable = ScheduleActionTableHelper.getInstance();
-	private CompanyInfoTableHelper companyInfoTable = CompanyInfoTableHelper.getInstance();
-	private DailyStockPriceDownloadAndStoreDBRunner runner1 = new DailyStockPriceDownloadAndStoreDBRunner();
-	private DailyStockPriceDownloadHelper2 sinaHelper2 = new DailyStockPriceDownloadHelper2();
-	private HistoryQianFuQuanStockPriceDownloadAndStoreDBRunner historyQianFuQuanRunner = new HistoryQianFuQuanStockPriceDownloadAndStoreDBRunner();
-	private String latestDate = "";
-	private int totalSize = 0;
+    // private static Logger logger =
+    // LogHelper.getLogger(DailyStockPriceDownloadAndStoreDBRunner2.class);
+    private CompanyInfoFileHelper stockConfig = CompanyInfoFileHelper.getInstance();
+    private StockPriceTableHelper stockPriceTable = StockPriceTableHelper.getInstance();
+    //private HouFuQuanStockPriceTableHelper houfuquanStockPriceTable = HouFuQuanStockPriceTableHelper.getInstance();
+    private QianFuQuanStockPriceTableHelper qianfuquanStockPriceTable = QianFuQuanStockPriceTableHelper.getInstance();
+    //private ScheduleActionTableHelper scheduleActionTable = ScheduleActionTableHelper.getInstance();
+    private CompanyInfoTableHelper companyInfoTable = CompanyInfoTableHelper.getInstance();
+    private DailyStockPriceDownloadAndStoreDBRunner runner1 = new DailyStockPriceDownloadAndStoreDBRunner();
+    private DailyStockPriceDownloadHelper2 sinaHelper2 = new DailyStockPriceDownloadHelper2();
+    private HistoryQianFuQuanStockPriceDownloadAndStoreDBRunner historyQianFuQuanRunner = new HistoryQianFuQuanStockPriceDownloadAndStoreDBRunner();
+    private String latestDate = "";
+    private int totalSize = 0;
 
-	// first download szzs, szcz, cybz,
-	// must record the latest date time
-	public void downloadMainBoardIndicator() {
-		List<String> stockIds = new ArrayList<String>();
+    // first download szzs, szcz, cybz,
+    // must record the latest date time
+    public void downloadMainBoardIndicator() {
+        List<String> stockIds = new ArrayList<String>();
 
-		stockIds.add(stockConfig.getSZZSStockIdForSina());
-		stockIds.add(stockConfig.getSZCZStockIdForSina());
-		stockIds.add(stockConfig.getCYBZStockIdForSina());
+        stockIds.add(stockConfig.getSZZSStockIdForSina());
+        stockIds.add(stockConfig.getSZCZStockIdForSina());
+        stockIds.add(stockConfig.getCYBZStockIdForSina());
 
-		runner1.downloadDataAndSaveIntoDB(stockIds);
-		// important: this json do not contain date information,
-		// just time is not enough, so we must get it form hq.sinajs.cn
-		// then query the database and get the latest deal date
-		this.latestDate = stockPriceTable.getLatestStockDate();
-	}
+        runner1.downloadDataAndSaveIntoDB(stockIds);
+        // important: this json do not contain date information,
+        // just time is not enough, so we must get it form hq.sinajs.cn
+        // then query the database and get the latest deal date
+        this.latestDate = stockPriceTable.getLatestStockDate();
+    }
 
-	public void downloadDataAndSaveIntoDB() {
+    public void downloadDataAndSaveIntoDB() {
 
-		if (Strings.isEmpty(this.latestDate)) {
-			System.out.println("Fatel Error, the latestDate is null! Return.");
-			return;
-		}
+        if (Strings.isEmpty(this.latestDate)) {
+            System.out.println("Fatel Error, the latestDate is null! Return.");
+            return;
+        }
 
-		System.out.println("Get stock price for " + this.latestDate);
+        System.out.println("Get stock price for " + this.latestDate);
 
-		List<SinaQuoteStockPriceVO> sqsList = sinaHelper2.fetchAllStockPriceFromWeb();
-		for (SinaQuoteStockPriceVO sqvo : sqsList) {
-			// to check if the stockId is a new on board one, if so, insert to
-			// companyInfo table
-			if (companyInfoTable.getCompanyInfoByStockId(sqvo.code) == null) {
-				CompanyInfoVO cinvo = new CompanyInfoVO(sqvo.code, sqvo.name);
-				companyInfoTable.insert(cinvo);
-				System.out.println("New company on board " + sqvo.code + " " + sqvo.name);
-			}
-			// convert to stockprice and save to DB
-			this.saveIntoDB(sqvo);
-		}
-	}
+        List<SinaQuoteStockPriceVO> sqsList = sinaHelper2.fetchAllStockPriceFromWeb();
+        for (SinaQuoteStockPriceVO sqvo : sqsList) {
+            // to check if the stockId is a new on board one, if so, insert to
+            // companyInfo table
+            CompanyInfoVO companyInfo = companyInfoTable.getCompanyInfoByStockId(sqvo.code);
+            if (companyInfo == null) {
+                CompanyInfoVO cinvo = new CompanyInfoVO(sqvo.code, sqvo.name);
+                companyInfoTable.insert(cinvo);
+                System.out.println("New company on board " + sqvo.code + " " + sqvo.name);
+            } else if (Strings.isNotEmpty(companyInfo.name) && !companyInfo.name.equals(sqvo.name)) {
+                //update the company name
+                System.out.println("Company change name from " + companyInfo.name + " to " + sqvo.name);
+                companyInfo.name = sqvo.name;
+                companyInfoTable.updateName(companyInfo);
+            }
+            // convert to stockprice and save to DB
+            this.saveIntoDB(sqvo);
+        }
+    }
 
-	public void saveIntoDB(SinaQuoteStockPriceVO sqvo) {
-		try {
-			// update stockprice into table
-			StockPriceVO spvo = new StockPriceVO();
-			spvo.stockId = sqvo.code;
-			spvo.name = sqvo.name;
-			// important: this json do not contain date information,
-			// just time is not enough, so we must get it form hq.sinajs.cn
-			spvo.date = this.latestDate;
-			spvo.close = sqvo.trade;
-			spvo.open = sqvo.open;
-			spvo.low = sqvo.low;
-			spvo.high = sqvo.high;
-			// sina data is 100 then sohu history data
-			spvo.volume = sqvo.volume / 100;
-			spvo.lastClose = sqvo.trade - sqvo.pricechange;
+    public void saveIntoDB(SinaQuoteStockPriceVO sqvo) {
+        try {
+            // update stockprice into table
+            StockPriceVO spvo = new StockPriceVO();
+            spvo.stockId = sqvo.code;
+            spvo.name = sqvo.name;
+            // important: this json do not contain date information,
+            // just time is not enough, so we must get it form hq.sinajs.cn
+            spvo.date = this.latestDate;
+            spvo.close = sqvo.trade;
+            spvo.open = sqvo.open;
+            spvo.low = sqvo.low;
+            spvo.high = sqvo.high;
+            // sina data is 100 then sohu history data
+            spvo.volume = sqvo.volume / 100;
+            spvo.lastClose = sqvo.trade - sqvo.pricechange;
 
-			// delete if today old data is exist
-			this.stockPriceTable.delete(spvo.stockId, spvo.date);
-			List<StockPriceVO> nDaySpList = this.stockPriceTable.getNdateStockPriceById(spvo.stockId, 1);
-			// System.out.println("saving into DB, vo=" + vo);
-			this.stockPriceTable.insert(spvo);
-			// also insert the qian fuquan stockprice
-			this.qianfuquanStockPriceTable.delete(spvo.stockId, spvo.date);
-			this.qianfuquanStockPriceTable.insert(spvo);
+            // delete if today old data is exist
+            this.stockPriceTable.delete(spvo.stockId, spvo.date);
+            List<StockPriceVO> nDaySpList = this.stockPriceTable.getNdateStockPriceById(spvo.stockId, 1);
+            // System.out.println("saving into DB, vo=" + vo);
+            this.stockPriceTable.insert(spvo);
+            // also insert the qian fuquan stockprice
+            this.qianfuquanStockPriceTable.delete(spvo.stockId, spvo.date);
+            this.qianfuquanStockPriceTable.insert(spvo);
 
-			// check if chu quan event exist
-			if (nDaySpList.size() > 0) {
-				StockPriceVO prevo = nDaySpList.get(0);
-				if (spvo.lastClose != 0 && prevo.close != 0 && spvo.lastClose != prevo.close) {
-					double rate = prevo.close / spvo.lastClose;
-					if (rate <= 0.95 || rate >= 1.05) {
-						// chu quan event
-						System.out.println("Chu Quan happens for " + spvo.stockId + ", rate=" + rate);
-						this.historyQianFuQuanRunner.countAndSave(spvo.stockId);
-					}
-				}
-			}
+            // check if chu quan event exist
+            if (nDaySpList.size() > 0) {
+                StockPriceVO prevo = nDaySpList.get(0);
+                if (spvo.lastClose != 0 && prevo.close != 0 && spvo.lastClose != prevo.close) {
+                    double rate = prevo.close / spvo.lastClose;
+                    if (rate <= 0.95 || rate >= 1.05) {
+                        // chu quan event
+                        System.out.println("Chu Quan happens for " + spvo.stockId + ", rate=" + rate);
+                        this.historyQianFuQuanRunner.countAndSave(spvo.stockId);
+                    }
+                }
+            }
 
-			this.totalSize++;
+            this.totalSize++;
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void run() {
-		downloadMainBoardIndicator();
-		downloadDataAndSaveIntoDB();
-		System.out.println("\ntotalSize=" + this.totalSize);
-	}
+    public void run() {
+        downloadMainBoardIndicator();
+        downloadDataAndSaveIntoDB();
+        System.out.println("\ntotalSize=" + this.totalSize);
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DailyStockPriceDownloadAndStoreDBRunner2 runner = new DailyStockPriceDownloadAndStoreDBRunner2();
-		runner.run();
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DailyStockPriceDownloadAndStoreDBRunner2 runner = new DailyStockPriceDownloadAndStoreDBRunner2();
+        runner.run();
+    }
 }
--- DatabaseLib/src/main/java/org/easystogu/db/access/view/CommonViewHelper.java
+++ DatabaseLib/src/main/java/org/easystogu/db/access/view/CommonViewHelper.java
@@ -15,145 +15,146 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class CommonViewHelper {
-	private static Logger logger = LogHelper.getLogger(CommonViewHelper.class);
-	private static CommonViewHelper instance = null;
-	private static CommonViewHelper georedInstance = null;
-	protected String QUERY_BY_VIEW_NAMES = "SELECT COUNT(*) AS rtn FROM pg_views WHERE VIEWNAME = :viewName";
+    private static Logger logger = LogHelper.getLogger(CommonViewHelper.class);
+    private static CommonViewHelper instance = null;
+    private static CommonViewHelper georedInstance = null;
+    protected String QUERY_BY_VIEW_NAMES = "SELECT COUNT(*) AS rtn FROM pg_views WHERE VIEWNAME = :viewName";
 
-	protected NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    protected NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-	public static CommonViewHelper getInstance() {
-		if (instance == null) {
-			instance = new CommonViewHelper(PostgreSqlDataSourceFactory.createDataSource());
-		}
-		return instance;
-	}
+    public static CommonViewHelper getInstance() {
+        if (instance == null) {
+            instance = new CommonViewHelper(PostgreSqlDataSourceFactory.createDataSource());
+        }
+        return instance;
+    }
 
-	public static CommonViewHelper getGeoredInstance() {
-		if (georedInstance == null) {
-			georedInstance = new CommonViewHelper(PostgreSqlDataSourceFactory.createGeoredDataSource());
-		}
-		return georedInstance;
-	}
+    public static CommonViewHelper getGeoredInstance() {
+        if (georedInstance == null) {
+            georedInstance = new CommonViewHelper(PostgreSqlDataSourceFactory.createGeoredDataSource());
+        }
+        return georedInstance;
+    }
 
-	protected CommonViewHelper(javax.sql.DataSource dataSource) {
-		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-	}
+    protected CommonViewHelper(javax.sql.DataSource dataSource) {
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
-	private static final class IntVOMapper implements RowMapper<Integer> {
-		public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return rs.getInt("rtn");
-		}
-	}
+    private static final class IntVOMapper implements RowMapper<Integer> {
+        public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return rs.getInt("rtn");
+        }
+    }
 
-	private static final class CommonViewVOMapper implements RowMapper<CommonViewVO> {
-		public CommonViewVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-			CommonViewVO vo = new CommonViewVO();
-			vo.setStockId(rs.getString("stockId"));
-			vo.setName(rs.getString("name"));
-			vo.setDate(rs.getString("date"));
-			return vo;
-		}
-	}
+    private static final class CommonViewVOMapper implements RowMapper<CommonViewVO> {
+        public CommonViewVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            CommonViewVO vo = new CommonViewVO();
+            vo.setStockId(rs.getString("stockId"));
+            vo.setName(rs.getString("name"));
+            vo.setDate(rs.getString("date"));
+            return vo;
+        }
+    }
 
-	private static final class CommonViewVOWithOutNameMapper implements RowMapper<CommonViewVO> {
-		public CommonViewVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-			CommonViewVO vo = new CommonViewVO();
-			vo.setStockId(rs.getString("stockId"));
-			// vo.setName(rs.getString("name"));
-			vo.setDate(rs.getString("date"));
-			return vo;
-		}
-	}
+    private static final class CommonViewVOWithOutNameMapper implements RowMapper<CommonViewVO> {
+        public CommonViewVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            CommonViewVO vo = new CommonViewVO();
+            vo.setStockId(rs.getString("stockId"));
+            // vo.setName(rs.getString("name"));
+            vo.setDate(rs.getString("date"));
+            return vo;
+        }
+    }
 
-	private static final class StringVOMapper implements RowMapper<String> {
-		public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return rs.getString("stockId");
-		}
-	}
+    private static final class StringVOMapper implements RowMapper<String> {
+        public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return rs.getString("stockId");
+        }
+    }
 
-	public boolean isViewExist(String viewName) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
-		namedParameters.addValue("viewName", viewName);
+    public boolean isViewExist(String viewName) {
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameters.addValue("viewName", viewName);
 
-		int rtn = this.namedParameterJdbcTemplate.queryForObject(QUERY_BY_VIEW_NAMES, namedParameters,
-				new IntVOMapper());
-		return rtn > 0 ? true : false;
-	}
+        int rtn = this.namedParameterJdbcTemplate.queryForObject(QUERY_BY_VIEW_NAMES, namedParameters,
+                new IntVOMapper());
+        return rtn > 0 ? true : false;
+    }
 
-	public List<CommonViewVO> queryByDateForViewDirectlySearch(String date, String viewName) {
-		try {
-			MapSqlParameterSource namedParameters = new MapSqlParameterSource();
-			StringBuffer sql = new StringBuffer();
-			sql.append("SELECT * FROM \"" + viewName + "\"");
-			//specific the date
-			if (Strings.isNotEmpty(date)) {
-				sql.append(" WHERE date = :date");
-				namedParameters.addValue("date", date);
-			} else if("Latest_20".equals(date)){
-			    //not specific the date, search least 20
-			    sql.append(" ORDER BY date DESC LIMIT 20");
-			}
-			
-			logger.info("queryByDateForViewDirectlySearch sql =" + sql.toString());
+    public List<CommonViewVO> queryByDateForViewDirectlySearch(String date, String viewName) {
+        try {
+            MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+            StringBuffer sql = new StringBuffer();
+            sql.append("SELECT * FROM \"" + viewName + "\"");
+            //specific the date
+            if ("Latest_20".equals(date)) {
+                //not specific the date, search least 20
+                sql.append(" ORDER BY date DESC LIMIT 20");
+            } else if (Strings.isNotEmpty(date)) {
+                sql.append(" WHERE date = :date");
+                namedParameters.addValue("date", date);
+            }
 
-			List<CommonViewVO> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
-					new CommonViewVOMapper());
-			return list;
+            logger.info("queryByDateForViewDirectlySearch sql =" + sql.toString());
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ArrayList<CommonViewVO>();
-	}
+            List<CommonViewVO> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
+                    new CommonViewVOMapper());
+            return list;
 
-	public List<CommonViewVO> queryByDateForCheckPoint(String viewName, String date) {
-		try {
-			MapSqlParameterSource namedParameters = new MapSqlParameterSource();
-			StringBuffer sql = new StringBuffer();
-			sql.append("SELECT * FROM \"" + viewName + "\"");
-			if (Strings.isNotEmpty(date)) {
-				sql.append(" WHERE date = :date");
-				namedParameters.addValue("date", date);
-			}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<CommonViewVO>();
+    }
 
-			List<CommonViewVO> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
-					new CommonViewVOWithOutNameMapper());
-			return list;
+    public List<CommonViewVO> queryByDateForCheckPoint(String viewName, String date) {
+        try {
+            MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+            StringBuffer sql = new StringBuffer();
+            sql.append("SELECT * FROM \"" + viewName + "\"");
+            if (Strings.isNotEmpty(date)) {
+                sql.append(" WHERE date = :date");
+                namedParameters.addValue("date", date);
+            }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ArrayList<CommonViewVO>();
-	}
+            List<CommonViewVO> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
+                    new CommonViewVOWithOutNameMapper());
+            return list;
 
-	public List<String> queryAllStockIds(String viewName) {
-		try {
-			MapSqlParameterSource namedParameters = new MapSqlParameterSource();
-			StringBuffer sql = new StringBuffer();
-			sql.append("SELECT stockId FROM \"" + viewName + "\"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<CommonViewVO>();
+    }
 
-			List<String> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
-					new StringVOMapper());
-			return list;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ArrayList<String>();
-	}
+    public List<String> queryAllStockIds(String viewName) {
+        try {
+            MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+            StringBuffer sql = new StringBuffer();
+            sql.append("SELECT stockId FROM \"" + viewName + "\"");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		CommonViewHelper ins = CommonViewHelper.getInstance();
-		List<CommonViewVO> list = ins.queryByDateForViewDirectlySearch("2016-09-20", "luzao_phaseII_zijinliu_top300_Details");
-		for (CommonViewVO vo : list) {
-			System.out.println(vo);
-		}
-		try {
-			System.out.println();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            List<String> list = this.namedParameterJdbcTemplate.query(sql.toString(), namedParameters,
+                    new StringVOMapper());
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<String>();
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        CommonViewHelper ins = CommonViewHelper.getInstance();
+        List<CommonViewVO> list = ins.queryByDateForViewDirectlySearch("2016-09-20",
+                "luzao_phaseII_zijinliu_top300_Details");
+        for (CommonViewVO vo : list) {
+            System.out.println(vo);
+        }
+        try {
+            System.out.println();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
