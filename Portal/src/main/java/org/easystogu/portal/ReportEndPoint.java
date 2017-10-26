32778de7a49be3a5333e67bfef4bfe7739fd8ec0
Albert.Yang <albert.yang@ericsson.com>
2017-10-26 10:49:41 +0800

    add one more check point

--- Portal/src/main/java/org/easystogu/portal/ReportEndPoint.java
+++ Portal/src/main/java/org/easystogu/portal/ReportEndPoint.java
@@ -10,6 +10,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
 import org.easystogu.cache.ConfigurationServiceCache;
+import org.easystogu.cache.StockPriceCache;
+import org.easystogu.config.Constants;
 import org.easystogu.db.access.view.FavoritesStockCheckpointViewHelper;
 import org.easystogu.db.vo.view.FavoritesStockCheckpointVO;
 import org.easystogu.utils.WeekdayUtil;
@@ -18,6 +20,7 @@ public class ReportEndPoint {
     private ConfigurationServiceCache config = ConfigurationServiceCache.getInstance();
     private FavoritesStockCheckpointViewHelper favoritesStockCheckpointViewHelper = FavoritesStockCheckpointViewHelper
             .getInstance();
+    private StockPriceCache stockPriceCache = StockPriceCache.getInstance();
     protected String accessControlAllowOrgin = config.getString("Access-Control-Allow-Origin", "");
 
     @GET
@@ -26,9 +29,10 @@ public class ReportEndPoint {
     //DateOffset is like 0,1,2,3 means today, yesterday, ...
     public String queryReportByDate(@PathParam("DateOffset") String dateOffset, @Context HttpServletResponse response) {
         response.addHeader("Access-Control-Allow-Origin", accessControlAllowOrgin);
-        String date = WeekdayUtil.nextNDate(WeekdayUtil.currentDate(), Integer.parseInt(dateOffset));
+        List<String> latestDates = this.stockPriceCache.get(Constants.cacheLatestNStockDate + ":10");
+        String date = latestDates.get(Math.abs(Integer.parseInt(dateOffset)));
         List<FavoritesStockCheckpointVO> list = favoritesStockCheckpointViewHelper.getByDateAndUserId(date, "admin");
-        StringBuffer rtn = new StringBuffer();
+        StringBuffer rtn = new StringBuffer("====" + date + "====<br>");
         for (FavoritesStockCheckpointVO vo : list) {
             rtn.append(vo.toString() + "<br>");
         }
