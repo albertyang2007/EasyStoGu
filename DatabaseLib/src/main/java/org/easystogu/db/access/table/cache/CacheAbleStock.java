2d1b89670e3621eb7dcb2678214615f4d9bbdd50
Albert.Yang <albert.yang@ericsson.com>
2017-11-22 11:27:22 +0800

    add sub module

--- DatabaseLib/src/main/java/org/easystogu/db/access/table/cache/CacheAbleStock.java
+++
@@ -1,9 +0,0 @@
-package org.easystogu.db.access.table.cache;
-
-import java.util.List;
-
-import org.easystogu.db.vo.table.StockPriceVO;
-
-public interface CacheAbleStock {
-	public List<StockPriceVO> queryByStockId(String stockId);
-}
