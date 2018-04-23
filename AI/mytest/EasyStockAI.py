f771dc03b52038c2f934b4098770adad0db49da6
Albert.Yang <albert.yang@ericsson.com>
2018-04-23 14:46:49 +0800

    add

--- AI/mytest/EasyStockAI.py
+++ AI/mytest/EasyStockAI.py
@@ -11,36 +11,36 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
 
-BASE_PATH = 'C:/Users/eyaweiw/github/EasyStoGu/CommonLib/src/main/resources/AI/'
+BASE_PATH = 'C:/Users/[path]/github/EasyStoGu/CommonLib/src/main/resources/AI/'
 # Load data
-szzs = pd.read_csv(os.path.join(BASE_PATH, "999999_high.csv"), usecols=[0,1])
-qsdd = pd.read_csv(os.path.join(BASE_PATH, "WR.csv"), usecols=[0,1,2,3])
+szzsHigh = pd.read_csv(os.path.join(BASE_PATH, "999999_high.csv"), usecols=[0,1])
+szzsLow = pd.read_csv(os.path.join(BASE_PATH, "999999_low.csv"), usecols=[0,1])
 
-# Make sure the data has same index (same date)
-idx_for_qsdd = qsdd['date'].isin(szzs['date']) 
-qsddFilter = qsdd[idx_for_qsdd]
+LuZaoCross = pd.read_csv(os.path.join(BASE_PATH, "LuZaoCross.csv"), usecols=[0,1,2,3,4,5])
+LuZaoTrend = pd.read_csv(os.path.join(BASE_PATH, "LuZaoTrend.csv"), usecols=[0,1,2,3,4])
+MACD = pd.read_csv(os.path.join(BASE_PATH, "MACD.csv"), usecols=[0,1,2])
+QSDD = pd.read_csv(os.path.join(BASE_PATH, "QSDD.csv"), usecols=[0,1,2,3])
+ShenXian = pd.read_csv(os.path.join(BASE_PATH, "ShenXian.csv"), usecols=[0,1,2])
+WR = pd.read_csv(os.path.join(BASE_PATH, "WR.csv"), usecols=[0,1,2,3])
 
-idx_for_szzs = szzs['date'].isin(qsddFilter['date'])
-szzsFilter = szzs[idx_for_szzs]
-
-forecastDay = 2
+forecastDay = 0
 # For szzs, only need second column, exclude first column (date)
-szzsData=np.array(szzsFilter)[:,1][0:len(szzsFilter)-forecastDay]
+szzsHighData=np.array(szzsHigh)[:,1][0:len(szzsHigh)-forecastDay]
+szzsLowData=np.array(szzsLow)[:,1][0:len(szzsLow)-forecastDay]
+
 # For qsdd, only need second column, exclude first column (date)
-qsddData=np.array(qsddFilter)[0:len(qsddFilter)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:6
+LuZaoCrossData=np.array(LuZaoCross)[0:len(LuZaoCross)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
+LuZaoTrendData=np.array(LuZaoTrend)[0:len(LuZaoTrend)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
+MACDData=np.array(MACD)[0:len(MACD)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
+QSDDData=np.array(QSDD)[0:len(QSDD)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
+ShenXianData=np.array(ShenXian)[0:len(ShenXian)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
+WRData=np.array(WR)[0:len(WR)-forecastDay:,1:6] # 0:len(qsddFilter)-2:,1:4
 
-print("qsdd:")
-print(qsddData)
-print("szzs:")
-print(szzsData)
+# using Classifier to fit the data
+clf = RandomForestClassifier().fit(QSDDData.astype('int'), szzsHighData.astype('int'))
+qsddP = [[46, 0, 8]] # 2018-01-29
+print("QSDD Predict: %d" % clf.predict(qsddP))# 1
 
-clf = RandomForestClassifier().fit(qsddData.astype('int'), szzsData.astype('int'))
-
-#qsdd = [[46, 0, 8]] # 2018-01-29
-#print(clf.predict(qsdd))# 1
-
-#luzao = [[36,160,704,310]] # 2018-01-29
-#print(clf.predict(luzao))# 1
-
-wr = [[5,13,171]] # 2018-01-29
-print(clf.predict(wr))# 1
+clf = RandomForestClassifier().fit(WRData.astype('int'), szzsHighData.astype('int'))
+wrP = [[5,13,171]] # 2018-01-29
+print("WR Predict: %d" % clf.predict(wrP))# 1
