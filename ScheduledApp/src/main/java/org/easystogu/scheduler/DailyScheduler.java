b2232aa8ccf2d4323d2afb41406664bc36bc0b87
AlbertYang <albert.yang@ericsson.com>
2020-10-17 08:53:42 +0800

    to fix the schedule webflux issue

--- ScheduledApp/src/main/java/org/easystogu/scheduler/DailyScheduler.java
+++ ScheduledApp/src/main/java/org/easystogu/scheduler/DailyScheduler.java
@@ -13,8 +13,6 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.core.publisher.Mono;
-
 @Configuration
 @EnableScheduling
 public class DailyScheduler implements SchedulingConfigurer {
@@ -33,39 +31,67 @@ public class DailyScheduler implements SchedulingConfigurer {
 	@Scheduled(cron = "0 31 11 * * MON-FRI")
 	public void dailyOverAllRunnerMorning() {
 		logger.info("dailyOverAllRunnerMorning start");
-		Mono<String> response = webClient.get().uri("/portal/scheduler/DailyOverAllRunner", 10L).accept(MediaType.TEXT_HTML)
-				.retrieve().bodyToMono(String.class);
-		logger.info("dailyOverAllRunnerMorning stop, response: {}", response.toString());
+		webClient.get()
+		    .uri("/portal/scheduler/DailyOverAllRunner", 10L)
+		    .accept(MediaType.TEXT_HTML)
+		    .exchange()
+            .flatMap( rtn -> {
+              logger.info("status code is :{}", rtn.statusCode());
+              return rtn.bodyToMono(String.class);
+            }).subscribe( v -> logger.info("response is: {}", v));
+		
+		logger.info("dailyOverAllRunnerMorning stop, waiting async handling result.");
 	}
 
 	// run at 15:06 DailyOverAllRunner
 	@Scheduled(cron = "0 06 15 * * MON-FRI")
 	public void dailyOverAllRunnerAndSanityAfternoon() {
 		logger.info("dailyOverAllRunnerAndSanityAfternoon start");
-		Mono<String> response = webClient.get().uri("/portal/scheduler/DailyOverAllRunnerAndSanity", 10L).accept(MediaType.TEXT_HTML)
-				.retrieve().bodyToMono(String.class);
-		logger.info("dailyOverAllRunnerAndSanityAfternoon stop, response: {}", response.toString());
+		webClient.get()
+		    .uri("/portal/scheduler/DailyOverAllRunnerAndSanity", 10L)
+		    .accept(MediaType.TEXT_HTML)
+            .exchange()
+            .flatMap( rtn -> {
+              logger.info("status code is :{}", rtn.statusCode());
+              return rtn.bodyToMono(String.class);
+            }).subscribe( v -> logger.info("response is: {}", v));
+		
+		logger.info("dailyOverAllRunnerAndSanityAfternoon stop, waiting async handling result.");
 	}
 
-	// run at 18:00
-	// @Scheduled(cron = "0 00 18 * * MON-FRI")
+	// run at 19:00
+	@Scheduled(cron = "0 00 19 * * MON-FRI")
 	public void dataBaseSanityCheck() {
 		logger.info("dataBaseSanityCheck start");
-		Mono<String> response = webClient.get().uri("/portal/scheduler/DataBaseSanityCheck", 10L).accept(MediaType.TEXT_HTML)
-				.retrieve().bodyToMono(String.class);
-		logger.info("dataBaseSanityCheck stop, response: {}", response.toString());
+		webClient.get()
+		    .uri("/portal/scheduler/DataBaseSanityCheck", 10L)
+		    .accept(MediaType.TEXT_HTML)
+            .exchange()
+            .flatMap( rtn -> {
+              logger.info("status code is :{}", rtn.statusCode());
+              return rtn.bodyToMono(String.class);
+            }).subscribe( v -> logger.info("response is: {}", v));
+		
+		logger.info("dataBaseSanityCheck stop, waiting async handling result.");
 	}
 
 	// sometime the stockprice has problem and will miss data or chuquan is not
 	// 每周更新一下stockprice，每次选择一部分
 	// please do not change the SAT-SUN, will impact the selected stockId
 	// 请不要随意更改这个时间，跟选出的stockid算法有关。
-	// run at 20:00 every day
-	// @Scheduled(cron = "0 00 20 * * ?")
+	// run at 21:00 every day
+	@Scheduled(cron = "0 00 21 * * ?")
 	public void dailyUpdateStockPriceByBatch() {
 		logger.info("dailyUpdateStockPriceByBatch start");
-		Mono<String> response = webClient.get().uri("/portal/scheduler/DailyUpdateStockPriceByBatch", 10L).accept(MediaType.TEXT_HTML)
-				.retrieve().bodyToMono(String.class);
-		logger.info("dailyUpdateStockPriceByBatch stop, response: {}", response.toString());
+		webClient.get()
+		    .uri("/portal/scheduler/DailyUpdateStockPriceByBatch", 10L)
+		    .accept(MediaType.TEXT_HTML)
+            .exchange()
+            .flatMap( rtn -> {
+              logger.info("status code is :{}", rtn.statusCode());
+              return rtn.bodyToMono(String.class);
+            }).subscribe( v -> logger.info("response is: {}", v));
+		
+		logger.info("dailyUpdateStockPriceByBatch stop, waiting async handling result.");
 	}
 }
